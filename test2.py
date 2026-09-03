3b3ac01df85760fe9cef068638845d123ff59a50
Amer Ayoub <amer_ayoub1@hotmail.com>
2026-09-03 22:17:15 +0200

    Kalkulator med dokumentering

---
+++ test2.py
@@ -0,0 +1,27 @@
+# legge sammen to tall
+def add(a, b):
+    return(a + b)
+
+# trekke to tall fra hverandre
+def sub(a, b):
+    return(a - b)
+
+# multiplisere to tall
+def mul(a, b):
+    return(a * b)
+
+#dividere to tall
+def div(a, b):
+    return(a / b)
+
+# lager verdien som funksjon retunerer i en ny variabel for hver funksjon av de fire funksjonen ovenpå
+svar1 = add(4, 90)
+svar2 = sub(4, 90)
+svar3 = mul(4, 90)
+svar4 = div(4, 90)
+
+# printe verdien av svar variablene
+print(svar1)
+print(svar2)
+print(svar3)
+print(svar4)
