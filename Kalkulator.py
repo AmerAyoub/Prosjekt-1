612a6b612b05302caafaea69b1aba307899d581f
Amer Ayoub <amer_ayoub1@hotmail.com>
2026-09-04 14:20:21 +0200

    Oppgave

---
+++ Kalkulator.py
@@ -0,0 +1,13 @@
+# lager 2 variabler der verdien inhenter fra testaturen
+input1 = int(input("skriv inn tallet_1: "))
+input2 = int(input("Skriv inn tallet_2: "))
+
+# legge sammen to tall 
+sum = (input1 + input2)
+
+# trekke den andre fra den første
+diff = (input1 + input2)
+
+#skriv ut 
+print({input1}, "+", {input2}, "=", {sum})
+print({input1}, "-", {input2}, "=", {diff})
