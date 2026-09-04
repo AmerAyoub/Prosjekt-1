ab02a0920eae0adf3dcb128538de8ff4b17b4210
Amer Ayoub <amer_ayoub1@hotmail.com>
2026-09-04 14:32:24 +0200

    refleksjon

--- Kalkulator.py
+++ Kalkulator.py
@@ -11,3 +11,15 @@ diff = (input1 + input2)
 #skriv ut 
 print({input1}, "+", {input2}, "=", {sum})
 print({input1}, "-", {input2}, "=", {diff})
+
+"""jeg har startet med å lage to variabler som er heltall.
+verdiene til variablene skal innhentes fra testaturen
+i starten har jeg ikke forstått hvorfor skal jeg lage to nye variabler
+som het sum og diff.
+men når jeg kom til utdata da har jeg forstått det.
+vi vill printe en regnstykke som a + b = c.
+jeg forstår ikke meaningen til parantesene {}.
+for å lage denne kalkulatoren har jeg ikke brukt KI, men jeg har 
+brukt den for å lage en annen kalkulator som gjør samme oppgaven.
+jeg brukte funksjonen def med return. her brukte jeg KI for å forstår 
+forskjellen mellom en variabel og parameter"""
